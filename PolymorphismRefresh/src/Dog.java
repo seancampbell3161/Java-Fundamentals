5fc690238c30ad0510a828b0e9302e528baa816f
Sean Campbell <seancampbell3161@gmail.com>
2021-06-06 17:14:35 -0500

    inherit from animal and implement Pet

--- PolymorphismRefresh/src/Dog.java
+++ PolymorphismRefresh/src/Dog.java
@@ -1,4 +1,4 @@
-public class Dog implements Animal {
+public class Dog extends Animal implements Pet {
 
     public void eat() {
         System.out.println("The dog eats");
@@ -6,5 +6,13 @@ public class Dog implements Animal {
 
     public void run() {
         System.out.println("The dog runs");
+    }
+
+    public void isNice() {
+        System.out.println("The dog wags its tail");
+    }
+
+    public void fetch() {
+        System.out.println("The dog fetches");
     }
 }
